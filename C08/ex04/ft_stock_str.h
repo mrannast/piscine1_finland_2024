46c18ae6880b13fcee2ddfd2987fbb0fb76ce6d9
mrannast <mrannast@student.hive.fi>
2024-01-31 20:59:52 +0200

    added udates in C08

---
+++ C08/ex04/ft_stock_str.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrannast <mrannast@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/31 19:52:40 by mrannast          #+#    #+#             */
+/*   Updated: 2024/01/31 20:44:51 by mrannast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
+typedef struct s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+}				t_stock_str;
+
+#endif
---
+++ C08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrannast <mrannast@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/31 19:33:13 by mrannast          #+#    #+#             */
+/*   Updated: 2024/01/31 20:56:20 by mrannast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strcpy(char *str)
+{
+	int		i;
+	char	*dest;
+	int		size;
+
+	if (!str)
+		return (NULL);
+	size = ft_strlen(str);
+	dest = (char *)malloc(size * sizeof(char));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		dest[i] = str[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+struct	s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int			i;
+	t_stock_str	*arr;
+
+	arr = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!arr)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		arr[i].size = ft_strlen(av[i]);
+		arr[i].str = av[i];
+		arr[i].copy = ft_strcpy(av[i]);
+		i++;
+	}
+	arr[i].size = 0;
+	arr[i].str = 0;
+	arr[i].copy = 0;
+	return (arr);
+}
